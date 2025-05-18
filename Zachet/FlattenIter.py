8c0a7551d21128a762d98962bb556d31cb70bcfa
admin <a@a.a>
2025-05-18 14:09:16 +0300

    fixed bug

--- Zachet/FlattenIter.py
+++ Zachet/FlattenIter.py
@@ -1,20 +1,21 @@
 class FlattenIterator:
     def __init__(self, nested_list):
-        self.stack = [nested_list]
+        self.nested = nested_list
 
     def __iter__(self):
-        return self
-
-    def __next__(self):
-        while self.stack:
-            current = self.stack.pop()
+        stack = [self.nested]
+        while stack:
+            current = stack.pop()
             if isinstance(current, list):
-                self.stack.extend(reversed(current))
+                stack.extend(reversed(current))
             else:
-                return current
-        raise StopIteration
+                yield current
+
 
 nested_list = [1, [2, [3, 4], 5], 6]
 flat = FlattenIterator(nested_list)
+for num in flat:
+    print(num)
+
 for num in flat:
     print(num)
