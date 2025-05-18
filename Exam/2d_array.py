c3f31130784aa941fd0fe0b760d8f1b6a89e4dcd
admin <a@a.a>
2025-05-18 14:44:34 +0300

    made 2d array task STREAM GNARLY!!!!

---
+++ Exam/2d_array.py
@@ -0,0 +1,21 @@
+def countNums(nums):
+    counter = {}
+    for num in nums:
+        if num not in counter.keys():
+            counter[num] = 1
+        else:
+            counter[num] += 1
+    return counter
+
+def twoDimArray(nums):
+    if not nums:
+        return []
+    count = dict(countNums(nums))
+    twodim = [[] for _ in range (max(count.values()))]
+    for value, frequency in count.items():
+        for row in range(frequency):
+            twodim[row].append(value)
+    return twodim
+
+print(twoDimArray([1,2,3,4,1,2,2,3]))
+
